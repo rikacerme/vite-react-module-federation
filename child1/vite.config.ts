dfadb4d6cb3b583a2a215fdee73ef3a4f9a61110
Team defineX <132335643+TeamDFX@users.noreply.github.com>
2025-02-16 01:40:17 +0300

    child1 serve with serve package

---
+++ child1/vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+//4174
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "remoteApp1",
+      filename: "remoteEntry.js", 
+      exposes: {
+        "./App": "./src/App", 
+      },
+      shared: ['react', "react-dom"]
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  },
+  server: {
+    cors: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*", 
+    },
+  },
+  preview: {
+    headers: {
+      "Access-Control-Allow-Origin": "*", 
+    },
+  },
+});
